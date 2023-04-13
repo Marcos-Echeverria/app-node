353a4f927d1ffadb7add557cdd94c3e5b65914f0
unknown <marcosalbertoecheverria@gmail.com>
2023-04-13 13:54:13 -0300

    agregando sales

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const debug = require('debug')('app:main');
 const {Config} = require('./src/config/index');
 const {ProductsAPI} = require('./src/products/index');
 const app = express();
+const {UsersAPI} = require('./src/users');
 
 app.use(express.json());
 
 //modulos
 ProductsAPI(app);
-
+UsersAPI(app);
 
 //
 app.listen(Config.port, () =>{
